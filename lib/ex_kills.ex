0d4accab7f8ce72a5871d65c616776626fc596f2
Ian Vaughan <github@ianvaughan.co.uk>
2020-06-10 21:09:08 +0100

    init

---
+++ lib/ex_kills.ex
@@ -0,0 +1,18 @@
+defmodule ExKills do
+  @moduledoc """
+  Documentation for `ExKills`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExKills.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_kills_test.exs
@@ -0,0 +1,8 @@
+defmodule ExKillsTest do
+  use ExUnit.Case
+  doctest ExKills
+
+  test "greets the world" do
+    assert ExKills.hello() == :world
+  end
+end
