feb5830e40e61375d34c4b36270f5d51fd706bc5
Ian Vaughan <github@ianvaughan.co.uk>
2020-06-12 17:01:33 +0100

    sub

---
+++ lib/ex_kills/kills.ex
@@ -0,0 +1,48 @@
+defmodule ExKills.Kills do
+  use WebSockex
+
+  @url "wss://zkillboard.com:2096"
+
+  def start_link do
+    {:ok, pid} = WebSockex.start_link(@url, __MODULE__, :no_state)
+    subscribe(pid)
+    {:ok, pid}
+  end
+
+  def handle_connect(conn, state) do
+    IO.puts "Connected!"
+    {:ok, state}
+  end
+
+  defp subscribtion_frame do
+    subscription_msg = %{
+      action: "sub",
+      channel: "none:*"
+    } |> Jason.encode!()
+
+    {:text, subscription_msg}
+  end
+
+  defp subscribe(pid) do
+    WebSockex.send_frame(pid, subscribtion_frame())
+    IO.puts "Subscribed!"
+  end
+
+  def handle_frame(_frame={:text, msg}, state) do
+    msg
+    |> Jason.decode!()
+    |> IO.inspect()
+    {:ok, state}
+  end
+end
+
+# %{
+#   "action" => "littlekill",
+#   "alliance_id" => 99006343,
+#   "character_id" => 2115132354,
+#   "corporation_id" => 98606096,
+#   "hash" => "bbc02a9401a18efbbfe1d81156d05fe96e9703ed",
+#   "killID" => 84906560,
+#   "ship_type_id" => 32878,
+#   "url" => "https://zkillboard.com/kill/84906560/"
+# }
--- mix.exs
+++ mix.exs
@@ -24,6 +24,14 @@ defmodule ExKills.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      # {:cowboy, "~> 2.4"},
+      # {:plug, "~> 1.7"},
+      # {:plug_cowboy, "~> 2.0"},
+      {:ecto_sql, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+
+      {:websockex, "~> 0.4.2"},
+      {:jason, "~> 1.1"}
     ]
   end
 end
