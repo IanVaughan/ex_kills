457a6b4501b692f2a9c185f6302b9cc6762274d2
Ian Vaughan <github@ianvaughan.co.uk>
2020-06-30 19:31:55 +0100

    Switch db

--- config/prod.exs
+++ config/prod.exs
@@ -4,5 +4,5 @@ config :ex_kills, Kills.Repo,
   username: "postgres",
   password: "123456",
   database: "ex_kills_repo",
-  socket_dir: "/tmp/cloudsql/ex-kills:europe-west2:ex-kills-db",
+  socket_dir: "/tmp/cloudsql/ex-kills:us-west1:ex-kills-db-us-west1",
   pool_size: 15
